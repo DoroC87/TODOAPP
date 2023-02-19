f2eeefdfb480ebb1125eec61b55443e3d30cb7d3
Gayoung Yang <dorothy8715@naver.com>
2023-02-19 23:11:08 +0900

    node.js part2

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
+
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+
+app.use("/public", express.static("public"));
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.kbtphjb.mongodb.net/todoapp?retryWrites=true&w=majority",
+  (e, client) => {
+    db = client.db("todoapp");
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+  }
+);
+
+app.get("/", function (req, res) {
+  res.render("index.ejs");
+});
+
+app.get("/write", function (req, res) {
+  res.render("write.ejs");
+});
+
+/**
+ * 할 일 submit 처리
+ * post collection insert
+ * @param req {title:오늘의 할일, date:날짜}
+ * @param res respons
+ */
+app.post("/add", (req, res) => {
+  db.collection("counter").findOne({ name: "countPost" }, (e, result) => {
+    if (e) return console.log(e);
+    var totalPost = result.totalPost;
+    db.collection("post").insertOne(
+      { _id: totalPost + 1, title: req.body.title, date: req.body.date },
+      (e, postResult) => {
+        db.collection("counter").updateOne(
+          { name: "countPost" },
+          { $inc: { totalPost: 1 } },
+          function (e, result) {
+            if (e) return console.log(e);
+            res.send("send complete");
+          }
+        );
+      }
+    );
+  });
+});
+
+/**
+ * 할 일 list find 처리
+ * post collection all find
+ * @param req
+ * @param res respons
+ */
+app.get("/list", (req, res) => {
+  db.collection("post")
+    .find()
+    .toArray(function (e, result) {
+      res.render("list.ejs", { posts: result });
+    });
+});
+
+app.delete("/delete", (req, res) => {
+  req.body._id = parseInt(req.body._id);
+  db.collection("post").deleteOne(req.body, function (e, result) {
+    res.status(200).send({ message: "succesed" });
+  });
+  // res.send("delete complete");
+});
+
+app.get("/detail/:id", (req, res) => {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    function (e, result) {
+      console.log(result);
+      res.render("detail.ejs", { data: result });
+    }
+  );
+});
